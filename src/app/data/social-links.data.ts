b608f4119cd0e43f4e1de132e3516dfc6bb08c1e
CalciteDragon <calcitedragon@gmail.com>
2026-03-22 23:45:53 -0700

    feat: add social links data

---
+++ src/app/data/social-links.data.ts
@@ -0,0 +1,19 @@
+import { SocialLink } from '../models/social-link.model';
+
+export const socialLinksData: readonly SocialLink[] = [
+  {
+    platform: 'github',
+    url: 'https://github.com/CalciteDragon',
+    label: 'GitHub',
+  },
+  {
+    platform: 'discord',
+    url: 'https://discord.com/users/calcite',
+    label: 'Discord',
+  },
+  {
+    platform: 'linkedin',
+    url: 'https://linkedin.com/in/tyler-hawthorn',
+    label: 'LinkedIn',
+  },
+];
