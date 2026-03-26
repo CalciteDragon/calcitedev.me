5d588d8d011ede8add83d07f9f61ee98854e49b5
CalciteDragon <calcitedragon@gmail.com>
2026-03-25 22:07:36 -0700

    refactor: move BackgroundSceneComponent to LayoutComponent for route persistence

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -7,7 +7,6 @@ import {
   inject,
 } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-import { BackgroundSceneComponent } from './background-scene/background-scene.component';
 import { HeroComponent } from './hero/hero.component';
 import { FeatureCardsComponent } from './feature-cards/feature-cards.component';
 import { ScrollRevealDirective } from '../../shared/directives/scroll-reveal.directive';
@@ -23,7 +22,6 @@ import { bioData } from '../../data/bio.data';
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
-    BackgroundSceneComponent,
     HeroComponent,
     FeatureCardsComponent,
     ScrollRevealDirective,
